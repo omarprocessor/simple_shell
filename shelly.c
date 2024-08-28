f2c3f43e8bc2b6214b4d910f47aa627eb56282b9
omarprocessor <wadaniinarais1@gmail.com>
2024-08-27 20:44:33 -0700

    corrected shelly and soma

--- shelly.c
+++ shelly.c
@@ -1,6 +1,4 @@
 #include "shelly.h"
-
-
 /**
  * execute_command - Forks a process and executes the command
  * @cmd: Command to execute
@@ -10,10 +8,8 @@ void execute_command(char *cmd)
 pid_t pid;
 int status;
 char *argv[2]; /* Declare the argv array with two elements */
-
 argv[0] = cmd; /* Assign the command as the first argument */
 argv[1] = NULL; /* The last element is NULL */
-
 pid = fork();
 if (pid == -1)
 {
@@ -24,10 +20,9 @@ else if (pid == 0)
 /* Child process executes the command */
 if (execve(cmd, argv, environ) == -1)
 {
-chora(cmd);
-chora("./shell: No such file or directory\n");
+chora("./hsh: No such file or directory\n");
 }
-exit(EXIT_FAILURE);
+_exit(EXIT_FAILURE); /* Use _exit instead of exit in child */
 }
 else
 {
@@ -35,7 +30,6 @@ else
 wait(&status);
 }
 }
-
 /**
  * main - Entry point for the simple shell
  *
@@ -44,22 +38,18 @@ wait(&status);
 int main(void)
 {
 char *cmd;
-
 while (1)
 {
 promptly();  /* Display the shell prompt */
-
 cmd = soma();  /* Read user input */
-
 if (cmd == NULL) /* Handle EOF (Ctrl+D) */
 {
 free(cmd);
 exit(0);
 }
-
 execute_command(cmd); /* Execute the command */
 free(cmd); /* Free the allocated memory for the command */
 }
-
 return (0);
 }
+
--- soma.c
+++ soma.c
@@ -1,5 +1,4 @@
 #include "shelly.h"
-
 /**
  * soma - Reads a line from standard input
  *
@@ -10,7 +9,6 @@ char *soma()
 size_t urefu = 0;
 char *laini = NULL;
 ssize_t soma = getline(&laini, &urefu, stdin);
-
 if (soma == -1)
 {
 if (feof(stdin))
@@ -21,14 +19,11 @@ exit(0);
 perror("getline");
 free(laini);
 exit(EXIT_FAILURE);
-
 }
 if (laini[soma - 1] == '\n')
 {
-
 laini[soma - 1] = '\0';
-
 }
 return (laini);
-
 }
+
