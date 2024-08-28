5fe524257ea26a6908031e9b6f38ad79fc432470
omarprocessor <wadaniinarais1@gmail.com>
2024-08-28 16:35:53 -0700

    corrected shugulikia_amri

--- shughulikia_amri.c
+++ shughulikia_amri.c
@@ -1,14 +1,18 @@
 #include "shelliyangu.h"
 
+/* Helper Function Prototypes */
+int handle_exit(char **vigezo);
+int handle_env(char **vigezo);
+int handle_command_execution(char **vigezo);
+
 /**
- * shughulikia_amri - Process and execute the user's command
- * @amri: The command entered by the user
+ * shughulikia_amri - Handles a command by breaking it down and executing
+ * @amri: The command string to handle
  *
- * Return: 1 to continue the loop, 0 to exit
+ * Return: 0 to exit the shell, 1 to continue
  */
 int shughulikia_amri(char *amri)
 {
-char *programu;
 char *vigezo[100];
 int i = 0;
 
@@ -16,25 +20,67 @@ vigezo[i] = strtok(amri, " ");
 while (vigezo[i] != NULL)
 vigezo[++i] = strtok(NULL, " ");
 
-if (vigezo[0] == NULL)
+if (handle_exit(vigezo) == 0)
+return (0);
+
+if (handle_env(vigezo) == 1)
 return (1);
 
+return (handle_command_execution(vigezo));
+}
+
+/**
+ * handle_exit - Checks if the command is "exit" and handles it
+ * @vigezo: Array of command arguments
+ *
+ * Return: 0 if the command is "exit", otherwise 1
+ */
+int handle_exit(char **vigezo)
+{
 if (strcmp(vigezo[0], "exit") == 0)
 return (0);
-if (strcmp(vigezo[0], "env") == 0)
-chapisha_env();
-return (1);
-programu = tafuta_njia(vigezo[0]);
-
-if (programu == NULL)
-{
-perror(vigezo[0]);
 return (1);
 }
 
-tekeleza_amri(programu, vigezo);
+/**
+ * handle_env - Checks if the command is "env" and prints the environment
+ * @vigezo: Array of command arguments
+ *
+ * Return: 1 if the command is "env", otherwise 0
+ */
+int handle_env(char **vigezo)
+{
+if (strcmp(vigezo[0], "env") == 0)
+{
+chapisha_env();
+return (1);
+}
+return (0);
+}
 
+/**
+ * handle_command_execution - Handles execution of a command
+ * @vigezo: Array of command arguments
+ *
+ * Return: 1 to continue the shell loop
+ */
+int handle_command_execution(char **vigezo)
+{
+char *programu;
+
+if (access(vigezo[0], X_OK) == 0)
+tekeleza_amri(vigezo[0], vigezo);
+else
+{
+programu = tafuta_njia(vigezo[0]);
+if (programu != NULL)
+{
+tekeleza_amri(programu, vigezo);
 free(programu);
+}
+else
+printf("%s: command not found\n", vigezo[0]);
+}
 return (1);
 }
 
