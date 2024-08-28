cc2cd855a44d55a4c262966df1a1245b2a8709c5
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-08-28 14:52:45 -0700

    add tekeleza_amri.c na shughulikia_amri.c

---
+++ shughulikia_amri.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * shughulikia_amri - Process and execute the user's command
+ * @amri: The command entered by the user
+ *
+ * Return: 1 to continue the loop, 0 to exit
+ */
+int shughulikia_amri(char *amri)
+{
+char *programu;
+char *vigezo[100];
+int i = 0;
+
+vigezo[i] = strtok(amri, " ");
+while (vigezo[i] != NULL)
+vigezo[++i] = strtok(NULL, " ");
+
+if (vigezo[0] == NULL)
+return (1);
+
+if (strcmp(vigezo[0], "exit") == 0)
+return (0);
+
+programu = tafuta_njia(vigezo[0]);
+
+if (programu == NULL)
+{
+perror(vigezo[0]);
+return (1);
+}
+
+tekeleza_amri(programu, vigezo);
+
+free(programu);
+return (1);
+}
+
---
+++ tekeleza_amri.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * tekeleza_amri - Execute the command using fork and execve
+ * @programu: The path to the executable program
+ * @vigezo: The command arguments
+ *
+ * Return: void
+ */
+void tekeleza_amri(char *programu, char *vigezo[])
+{
+pid_t mchakato_id;
+int hali;
+
+mchakato_id = fork();
+if (mchakato_id == 0)
+{
+if (execve(programu, vigezo, NULL) == -1)
+perror("execve");
+exit(EXIT_FAILURE);
+}
+else if (mchakato_id < 0)
+{
+perror("fork");
+}
+else
+{
+waitpid(mchakato_id, &hali, 0);
+}
+}
